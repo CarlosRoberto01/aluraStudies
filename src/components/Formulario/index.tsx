75149bb1f2e9bca9fe0065e85030bab2389fcbe0
Carlos Roberto <carloos.bezerra@hotmail.com>
2023-09-18 17:45:05 -0300

    Controlando o formulário

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,11 +1,25 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import Botao from '../Botao'
 import style from './Formulario.module.scss'
 
+
+
 export default function Formulario() {
+    const [tarefaPadrao, setTarefaPadrao] = useState(
+        {
+        tarefa:"",
+        tempo:"00:00:00"
+        }     
+    )
+    
+    function adicionarTarefa(event: React.FormEvent<HTMLFormElement>){
+        event.preventDefault();
+        console.log("peguei")
+    }
+
     return (
-        <form className={style.novaTarefa}>
+        <form className={style.novaTarefa} onSubmit={adicionarTarefa}>
             <div className={style.inputContainer}>
                 <label htmlFor='tarefa'>
                     Adicione um novo estudo
@@ -13,6 +27,9 @@ export default function Formulario() {
                 <input
                     type='text'
                     name='tarefa'
+                    id='tarefa'
+                    value={tarefaPadrao.tarefa}
+                    onChange={evento=>setTarefaPadrao({...tarefaPadrao,tarefa: evento.target.value})}
                     placeholder='O que você quer estudar?'
                     required
                 />
@@ -25,6 +42,8 @@ export default function Formulario() {
                     type='time'
                     step='1'
                     name='tempo'
+                    value={tarefaPadrao.tempo}
+                    onChange={evento => setTarefaPadrao({...tarefaPadrao, tempo: evento.target.value})}
                     id='tempo'
                     min='00:00:00'
                     max='01:30:00'
